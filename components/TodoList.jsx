5f39c06dd0b084016055625baab40757a8bb81bc
nhinhiii <chaochochochao@gmail.com>
2024-07-01 19:31:00 +0700

    Test: - Show action state of the tasks such as all tasks, WIP or completed tasks - Add, delete, move up/down

--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -1,20 +1,24 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-import { Box, Button, Text, Input, List, ListItem, IconButton, HStack } from '@chakra-ui/react';
-import { DeleteIcon } from '@chakra-ui/icons';
+import { Box, Button, Text, Input, List, ListItem, IconButton, HStack, Menu, MenuButton, MenuList, MenuItem, Flex, VStack } from '@chakra-ui/react';
+import { DeleteIcon, ChevronDownIcon, EditIcon } from '@chakra-ui/icons';
 import { FaArrowCircleUp, FaArrowAltCircleDown } from "react-icons/fa";
-import { Checkbox, CheckboxGroup } from '@chakra-ui/react'
+import { Checkbox } from '@chakra-ui/react'
 
 const TodoList = () => {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
+  const [action, setAction] = useState("All");
 
   //load
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const savedTasks = localStorage.getItem('tasks');
+      //error forgot to convert to String 
       if (savedTasks) {
-        setTasks(JSON.parse(savedTasks));
+        const parsedTasks = JSON.parse(savedTasks);
+        const updatedTasks = parsedTasks.map(task => typeof task === 'string' ? { text: task, checked: false} : task); 
+        setTasks(updatedTasks);
       } 
     }
   }, []); 
@@ -33,7 +37,7 @@ const TodoList = () => {
   function addTask() {
     //empty string??
     if (newTask.trim()) {
-      setTasks(task => [...task, newTask]);
+      setTasks([...tasks, {text: newTask, check: false}]);
       setNewTask("");
     }
   }
@@ -62,25 +66,78 @@ const TodoList = () => {
     }
   }
 
+  //ensure the checbox follows corrct tasks
+  //check index
+  const checkBox = (index) => {
+    const updatedTasks =[...tasks];
+    updatedTasks[index].checked = !updatedTasks[index].checked;
+    setTasks(updatedTasks);
+  };
+
+  //all, WIP, completed 
+  //check state 
+  const setCheckAction = (checkAction) => {
+    setAction(checkAction);
+  }
+
+  const stateTasks = tasks.filter(task => {
+    if (action === "All") return true;
+    if (action === "In Progress")return !task.checked;
+    if (action === "Completed") return task.checked;
+    return true;
+  });
+
+
   return (
     <Box textAlign="center" mx="200px" my="100px" px={4}>
-      <Text as='b' fontSize="5xl" fontFamily="Arial, sans-serif" mb={4}>To Do List</Text>
+      <Flex alignItems="center" justifyContent="space-between" mb={5}>
+        <Box flex="1">
+          <Text as='b' textAlign="center" fontSize="5xl" fontFamily="Arial, sans-serif">To Do List</Text>
+        </Box>
+        <Box>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} border="1px solid black">
+              Actions
+            </MenuButton>
+            <MenuList>
+              <MenuItem onClick={() => setCheckAction("All")}>All</MenuItem>
+              <MenuItem onClick={() => setCheckAction("In Progress")}>In Progress</MenuItem>
+              <MenuItem onClick={() => setCheckAction("Completed")}>Completed</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+      </Flex>
       <HStack mb={4} spacing={2}>
         <Input
           type="text"
           placeholder="Enter your task..."
           value={newTask}
           onChange={handleInputChange}
+          autoComplete="off"
         />
         <Button onClick={addTask} colorScheme="teal">Add</Button>
       </HStack>
 
       <List spacing={3}>
-        {tasks.map((task, index) => (
-          <ListItem key={index} display="flex" alignItems="center" justifyContent="space-between" border="2px solid black" borderRadius="5px" p="10px">
+        {stateTasks.map((task, index) => (
+          <ListItem 
+            key={index} 
+            display="flex" 
+            alignItems="center" 
+            justifyContent="space-between" 
+            border="2px solid black" 
+            borderRadius="5px" 
+            p="10px">
+
             <Box alignItems="center" justifyContent="space-between" display="flex" m="5px">
-              <Checkbox defaultChecked colorScheme ="green"></Checkbox>
-              <Text as='b'>{task}</Text>
+              <Checkbox 
+                colorScheme="green" 
+                size="md" 
+                mr={3}
+                isChecked={task.checked}
+                onChange={() => checkBox(index)}
+                />
+              <Text as='b' textDecoration={task.checked ? "line-through" : "none"}>{task.text}</Text>
             </Box>
             
             <HStack spacing={2}>
@@ -111,3 +168,4 @@ const TodoList = () => {
 }
 
 export default TodoList;
+
